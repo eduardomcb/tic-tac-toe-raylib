b90fa04f45349c1e797b875db523dd76c70d280d
Batalha <ericksilva6570@gmail.com>
2025-10-23 17:27:05 -0300

    adicionado o botao placar e sair

--- src/main.c
+++ src/main.c
@@ -22,10 +22,17 @@ int main(void) {
   float btnWidth = game->texButton.width;
   float btnHeight = game->texButton.height;
 
-  Rectangle playBounds = {(SCREEN_WIDTH / 2.0f) - (btnWidth / 2.0f),
-                          SCREEN_HEIGHT / 2.0f - btnHeight * 1.5f, btnWidth,
-                          btnHeight};
+  //Botão iniciar
+  Rectangle playBounds = {(SCREEN_WIDTH / 2.0f) - (btnWidth / 2.0f),SCREEN_HEIGHT / 2.0f - btnHeight * 1.5f, btnWidth,btnHeight};
   InitButton(&game->btnPlay, game->texButton, playBounds, "Iniciar Jogo");
+
+  //Botão Placar
+  Rectangle scoreBounds = {playBounds.x , playBounds.y + playBounds.height + 20.0f, btnWidth, btnHeight};
+  InitButton(&game->btnScoreboard, game->texButton, scoreBounds, "Placar");
+
+  //Botão Sair
+  Rectangle exitBounds = {scoreBounds.x , scoreBounds.y + scoreBounds.height + 20.0f,btnWidth, btnHeight};
+  InitButton(&game->btnExit, game->texButton, exitBounds, "Sair");
 
   while (!WindowShouldClose()) {
 
@@ -34,7 +41,16 @@ int main(void) {
     case SCREEN_MENU:
       mousePoint = GetMousePosition();
 
+      Vector2 textSize = MeasureTextEx(game -> font, "Tic  tac  toe - UFAL", 60.0f, 1.0f);
+      float textWidth = textSize.x;
+
+      float textPosX = (SCREEN_WIDTH / 2.0f) - (textWidth / 2.0f);
+
+      DrawTextEx(game -> font, "Tic  tac  toe - UFAL", (Vector2){textPosX, 50},60.0f,1.0f,DARKGREEN);
+
       UpdateButton(&game->btnPlay, mousePoint);
+      UpdateButton(&game->btnScoreboard, mousePoint);
+      UpdateButton(&game->btnExit, mousePoint);
       break;
     case SCREEN_PLAYING:
 
@@ -52,6 +68,8 @@ int main(void) {
     switch (game->currentScreen) {
     case SCREEN_MENU:
       DrawButton(&game->btnPlay, game->font, BUTTON_TEXT_FONT_SIZE, 1.0f);
+      DrawButton(&game->btnScoreboard, game->font, BUTTON_TEXT_FONT_SIZE, 1.0f);
+      DrawButton(&game->btnExit, game->font, BUTTON_TEXT_FONT_SIZE, 1.0f);
       break;
     case SCREEN_PLAYING:
       DrawText("Tela da jogatinha", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 24.0f,
