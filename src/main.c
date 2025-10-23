dc24b3e2dff6de2e25b944d93f69044fe6a47d6d
eduardomcb <eduardomateuscostab@gmail.com>
2025-10-23 15:35:17 -0300

    test: Implementing button functions in main

--- src/main.c
+++ src/main.c
@@ -1,67 +1,43 @@
+#include "button.h"
 #include "constants.h"
 #include "raylib.h"
 #include "types.h"
-
-//botão de exemplo
-bool button(Rectangle bounds, const char *text, Texture2D texture, Font font,
-            float fontSize, Color colorText) {
-  Vector2 mousePoint = GetMousePosition();
-  ButtonState state = BTN_STATE_NORMAL;
-  bool action = false;
-
-  if (CheckCollisionPointRec(mousePoint, bounds)) {
-    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-      state = BTN_STATE_PRESSED;
-    else
-      state = BTN_STATE_HOVER;
-    if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
-      action = true;
-  }
-
-  Color tint = WHITE;
-  if (state == BTN_STATE_HOVER)
-    tint = LIGHTGRAY;
-  if (state == BTN_STATE_PRESSED)
-    tint = DARKGRAY;
-  DrawTexture(texture, (int)bounds.x, (int)bounds.y, tint);
-
-  // float fontSize = 40.0f;
-  float spacing = 1.0f;
-
-  Vector2 textSize = MeasureTextEx(font, text, fontSize, spacing);
-
-  float textPosX = (bounds.x + (bounds.width / 3.f)) - (textSize.x / 2.0f);
-  float textPosY = (bounds.y + (bounds.height / 3.f)) - (textSize.y / 2.0f);
-
-  DrawTextEx(font, text, (Vector2){textPosX, textPosY}, fontSize, spacing,
-             colorText);
-
-  return action;
-}
+#include <stdlib.h>
 
 int main(void) {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE);
 
-  Font kenneyFont = LoadFont("assets/fonts/sua_fonte_do_kenney.ttf");
-  Texture2D texButton =
+  Vector2 mousePoint = {0.0f, 0.0f};
+
+  GameState *game = malloc(sizeof(GameState));
+  game->font = LoadFont("assets/fonts/kenney-future-narrow.ttf");
+  game->texButton =
       LoadTexture("assets/images/ui/button_rectangle_depth_flat.png");
 
-  Rectangle btnStartBounds = {(SCREEN_WIDTH / 2.0f) - (texButton.width / 2.0f),
-                              SCREEN_HEIGHT / 2.0f - texButton.height - 10,
-                              (float)texButton.width, (float)texButton.height};
+  float btnWidth = game->texButton.width;
+  float btnHeight = game->texButton.height;
+
+  Rectangle playBounds = {(SCREEN_WIDTH / 2.0f) - (btnWidth / 2.0f),
+                          SCREEN_HEIGHT / 2.0f - btnHeight * 1.5f, btnWidth,
+                          btnHeight};
+  InitButton(&game->btnPlay, game->texButton, playBounds, "Iniciar Jogo");
 
   while (!WindowShouldClose()) {
-    BeginDrawing();
 
+    mousePoint = GetMousePosition();
+
+    UpdateButton(&game->btnPlay, mousePoint);
+    BeginDrawing();
     ClearBackground(RAYWHITE);
 
-    if (button(btnStartBounds, "Começar", texButton, kenneyFont, 40, WHITE)) {
-      TraceLog(LOG_INFO, "A Partida vai começar");
-    }
+    DrawButton(&game->btnPlay, game->font, BUTTON_TEXT_FONT_SIZE, 1.0f);
 
     EndDrawing();
   }
 
+  UnloadTexture(game->texButton);
+  UnloadFont(game->font);
+  free(game);
   CloseWindow();
 
   return 0;
