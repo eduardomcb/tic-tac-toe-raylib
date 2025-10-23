5380e08273d5ee797cd420ab70a5b850d70df851
eduardomcb <eduardomateuscostab@gmail.com>
2025-10-23 16:04:32 -0300

    feat: Add screen switching

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 
 int main(void) {
+
+  /*antes do while temos as funções, estruturas e variaveis que precisam ser
+    inicaalizadas*/
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE);
 
   Vector2 mousePoint = {0.0f, 0.0f};
@@ -13,6 +16,8 @@ int main(void) {
   game->font = LoadFont("assets/fonts/kenney-future-narrow.ttf");
   game->texButton =
       LoadTexture("assets/images/ui/button_rectangle_depth_flat.png");
+
+  game->currentScreen = SCREEN_MENU;
 
   float btnWidth = game->texButton.width;
   float btnHeight = game->texButton.height;
@@ -24,13 +29,43 @@ int main(void) {
 
   while (!WindowShouldClose()) {
 
-    mousePoint = GetMousePosition();
+    // switch para funções de updates para cada tela/etapa
+    switch (game->currentScreen) {
+    case SCREEN_MENU:
+      mousePoint = GetMousePosition();
 
-    UpdateButton(&game->btnPlay, mousePoint);
+      UpdateButton(&game->btnPlay, mousePoint);
+      break;
+    case SCREEN_PLAYING:
+
+      break;
+    case SCREEN_SCOREBOARD:
+      break;
+    case SCREEN_OVER:
+      break;
+    }
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
-    DrawButton(&game->btnPlay, game->font, BUTTON_TEXT_FONT_SIZE, 1.0f);
+    // switch para desenhar na tela para cada tela/etapa
+    switch (game->currentScreen) {
+    case SCREEN_MENU:
+      DrawButton(&game->btnPlay, game->font, BUTTON_TEXT_FONT_SIZE, 1.0f);
+      break;
+    case SCREEN_PLAYING:
+      DrawText("Tela da jogatinha", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 24.0f,
+               BLACK);
+      break;
+    case SCREEN_SCOREBOARD:
+      DrawText("Tela de visualizar placar", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+               24.0f, BLACK);
+      break;
+    case SCREEN_OVER:
+      DrawText("Tela de fim de jogo", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+               24.0f, BLACK);
+      break;
+    }
 
     EndDrawing();
   }
